dce6cb0f5be14a2acad74c11900f9149d92e2196
Uthman <uthmanigbin@gmail.com>
2025-02-06 01:59:15 +0200

    changed up dark theme color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,23 +10,23 @@ export default {
   theme: {
     extend: {
       colors: {
-        lightHover: '#fcf4ff',
+        lightHover: "#fcf4ff",
         darkHover: "#2a004a",
-        darkTheme: '#11001f',
+        darkTheme: "#02091C",
       },
       fontFamily: {
         Outfit: ["Outfit", "sans-serif"],
         Ovo: ["Ovo", "serif"],
       },
       boxShadow: {
-        'black': '4px 4px 0 #000',
-        'white': '4px 4px 0 #fff',
+        black: "4px 4px 0 #000",
+        white: "4px 4px 0 #fff",
       },
       gridTemplateColumns: {
-        'auto': 'repeat(auto-fit, minmax(200px, 1fr))',
+        auto: "repeat(auto-fit, minmax(200px, 1fr))",
       },
     },
   },
-  darkMode: 'selector',
+  darkMode: "selector",
   plugins: [],
 };
