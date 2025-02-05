01912a4cde940241b412ff86edb60d3a51943b92
Uthman <uthmanigbin@gmail.com>
2025-02-06 00:58:07 +0200

    updated info list

--- assets/assets.js
+++ assets/assets.js
@@ -107,9 +107,9 @@ export const serviceData = [
 ]
 
 export const infoList = [
-    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML, CSS, JavaScript React Js, Next Js' },
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'JavaScript, ReactJs, NextJs, NodeJs, Python, PHP' },
     { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.Tech in Computer Science' },
-    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects' }
+    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Experience', description: 'Been programming since I was 7 and have multiple hackathon wins under my belt' }
 ];
 
 export const toolsData = [
